7067d7dcc0bd618755ae9891bd7f04f36df57f68
reinaldariel <reinald.a.k@gmail.com>
2019-05-18 15:06:09 +0700

    Update hapus_lahan.php
    menghapus detil2 yg berhubungan dengan lahan terlebih dahulu

--- service/hapus_lahan.php
+++ service/hapus_lahan.php
@@ -5,6 +5,35 @@ $conn = $database->getConnection();
 $id_lahan = $_GET['id_lahan'];
 
 try {
+    //hapus kepemilikan lahan
+    $stmt = $conn->prepare("DELETE FROM trans_lahan WHERE ID_Lahan = ?");
+    $stmt->bindParam(1, $id_lahan);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+
+    //hapus detil titik lahan
+    $stmt = $conn->prepare("DELETE FROM master_peta_lahan_detail WHERE id_lahan = ?");
+    $stmt->bindParam(1, $id_lahan);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+
+    //hapus penanaman lahan
+    $stmt = $conn->prepare("DELETE FROM master_peta_lahan_tanaman WHERE ID_Lahan = ?");
+    $stmt->bindParam(1, $id_lahan);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+
+    //hapus foto lahan
+    $stmt = $conn->prepare("DELETE FROM master_peta_lahan_foto WHERE ID_Lahan = ?");
+    $stmt->bindParam(1, $id_lahan);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+
+    //hapus lahan
     $stmt = $conn->prepare("DELETE FROM master_peta_lahan WHERE ID_Lahan = ?");
     $stmt->bindParam(1, $id_lahan);
     $stmt->execute();
