6db6e77a10983f45e47a637e99f2d43a7839e1a2
reinaldariel <reinald.a.k@gmail.com>
2019-06-09 00:24:00 +0700

    Update hapus_penanaman.php
    bug fixing salah redirect

--- service/hapus_penanaman.php
+++ service/hapus_penanaman.php
@@ -4,14 +4,13 @@ $database = new Database();
 $conn = $database->getConnection();
 $id_penanaman = $_GET['id_penanaman'];
 $id_lahan = "";
+
 $stmt = $conn->prepare("SELECT ID_Lahan FROM master_peta_lahan_tanaman WHERE id_detail_tanaman = ?");
 $stmt->bindParam(1, $id_penanaman);
 $stmt->execute();
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
-while($id_lahan = $stmt->fetch(PDO::FETCH_ASSOC)){
-$id_lahan = $id_lahan['ID_Lahan'];
-}
-
+$id_lahan = $stmt->fetchAll();
+$id_lahan = $id_lahan[0]['ID_Lahan'];
 
 try {
     $stmt = $conn->prepare("DELETE FROM master_peta_lahan_tanaman WHERE id_detail_tanaman = ?");
