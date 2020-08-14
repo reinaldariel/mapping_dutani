e2be336fa71a6e9918ed7647affefb336cc74806
Argo Wibowo <argo.wibowo1989@gmail.com>
2020-08-14 09:57:32 +0700

    open all kelompok tani

--- service/read_klptani.php
+++ service/read_klptani.php
@@ -5,13 +5,13 @@ $database = new Database();
 $conn = $database->getConnection();
 //$str = "SELECT DISTINCT tp.ID_Kelompok_Tani as id, k.nama_kelompok_tani from master_kel_tani k, trans_ang_petani tp, master_petani p, trans_lahan tl, master_peta_lahan l where k.ID_Kelompok_Tani = tp.ID_Kelompok_Tani AND tp.ID_User = p.ID_User AND p.ID_User = tl.ID_User AND tl.ID_Lahan = l.ID_Lahan";
 $str = "SELECT DISTINCT l.ID_Kelompok_Tani as id, k.nama_kelompok_tani from master_kel_tani k, master_peta_lahan l, trans_lahan tl where k.ID_Kelompok_Tani = l.ID_Kelompok_Tani AND tl.ID_Lahan = l.ID_Lahan";
-if (isset($_GET['klptani'])){
+/*if (isset($_GET['klptani'])){
 //    $str.= " and tp.ID_Kelompok_Tani != '".$_GET['klptani']."'";
     $str.= " and l.ID_Kelompok_Tani != '".$_GET['klptani']."'";
 }
 elseif (isset($_GET['idp'])){
     $str.= " and tl.ID_User != '".$_GET['idp']."'";
-}
+}*/
 try {
     $stmt = $conn->prepare($str);
     $stmt->execute();
